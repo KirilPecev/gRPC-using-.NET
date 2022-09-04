24a9ab57078a972dcbf3c4dbd43707d846e9083d
Kiril Pecev <kir4o_pecev@abv.bg>
2022-09-04 17:48:10 +0300

    Initial setup

---
+++ gRPC/gRPCClient/Program.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+using GrpcService;
+
+HelloRequest message = new() { Name = "MyGRPC" };
+GrpcChannel channel = GrpcChannel.ForAddress("https://localhost:7057");
+Greeter.GreeterClient client = new Greeter.GreeterClient(channel);
+HelloReply serverReply = await client.SayHelloAsync(message);
+Console.WriteLine(serverReply.Message);
+
